00acf6a6f055fd09a906604eb8c7cb27d6f99d9e
xsmallninja <xsmallninja@gmail.com>
2021-09-29 06:36:53 -0500

    NC: Minor adjustments.

--- train_source_models.sh
+++ train_source_models.sh
@@ -8,7 +8,10 @@ for value in {0..2}; do
   python ./train_classifier.py \
   --n_target_samples $n_target_samples \
   --target_batch_size $tgt_batch_size \
-  --exp_dir baseline_s_20k_t_$n_target_samples \
+  --exp_dir film_s_20k_t_$n_target_samples \
   --iter_idx $value \
-  --train_seed ${train_seeds[$value]};
+  --train_seed ${train_seeds[$value]} \
+  --freeze \
+  --fine_tune_layers resnet_fc linear \
+  --film;
 done
