7b5b5bbf4fc3da371723339b8dd82eedeb72b3af
xsmallninja <xsmallninja@gmail.com>
2021-10-22 22:45:07 -0500

    NC: Initial commit of example training scripts and batch job submission file.

---
+++ batch_jobs/model_scripts/train_source_models.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+root_dir='/home/nschiou2/domain_adapt_cxr/'
+results_dir='/shared/rsaas/nschiou2/domain_adapt_cxr/fine_tuning/'
+
+cd $root_dir
+
+src_batch_size=16
+tgt_batch_size=16
+n_target_samples=20
+train_seeds=(8 16 31)
+
+for value in {0..2}; do
+  pipenv run python ./train_classifier.py \
+  --early_stop \
+  --n_target_samples $n_target_samples \
+  --source_batch_size $src_batch_size \
+  --target_batch_size $tgt_batch_size \
+  --exp_dir $results_dir/baseline/fixed_batch_size/baseline_s_20k_t_$n_target_samples \
+  --iter_idx $value \
+  --train_seed ${train_seeds[$value]};
+done
