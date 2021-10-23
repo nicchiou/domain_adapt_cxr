eaf3e879a1966f4d9497f068eb920ab0a821c800
xsmallninja <xsmallninja@gmail.com>
2021-10-22 22:46:25 -0500

    NC: Adding changes to file structure.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="domain_adapt_cxr",
+    version="0.0",
+    author="Nicole Chiou",
+    author_email="nschiou2@illinois.edu",
+    packages=['models', 'utils'],
+)
