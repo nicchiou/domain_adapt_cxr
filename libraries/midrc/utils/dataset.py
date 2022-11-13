4911463590ea345544b7b9596918b01f017b1d08
Nicole Chiou <nicchiou@hyperion3.stanford.edu>
2022-11-13 09:28:42 -0800

    NC: Improve MIDRC dataset to load all data initially.

--- libraries/midrc/utils/dataset.py
+++ libraries/midrc/utils/dataset.py
@@ -1,7 +1,5 @@
 """PyTorch-loadable dataset with MIDRC CXR data."""
-import os
-
-import sklearn.model_selection
+from sklearn.model_selection import train_test_split
 import pandas as pd
 from PIL import Image
 import torch
@@ -10,39 +8,47 @@ from torch.utils.data import Dataset
 class MIDRCDataset(Dataset):
     """MIDRC dataset."""
     def __init__(self, metadata_path, n_samples=None, transform=None, seed=0):
+        self.n_samples = n_samples
         self.transform = transform
+        self.seed = seed
 
-        df = pd.read_csv(metadata_path)
-        df.loc[:, 'covid19_positive'] = (
-            df['covid19_positive'] == 'Yes').astype(int)
+        self.data = pd.read_csv(metadata_path)
+        self.data.loc[:, 'covid19_positive'] = (
+            self.data['covid19_positive'] == 'Yes').astype(int)
 
-        self.image_paths = df.file_path.values
-        self.labels = df.covid19_positive.values
+        images, labels = [], []
+        for i in range(len(self.data)):
+            image, label = self.__loaditem__(i)
+            images.append(image)
+            labels.append(label)
+
+        self.images = torch.stack(images, 0)
+        self.labels = torch.tensor(labels)
 
         if n_samples is not None:
             # Randomly select subset of samples, stratified by label
             n_remove = int(len(self.labels) - n_samples)
-            idx, _ = sklearn.model_selection.train_test_split(
+            idx, _ = train_test_split(
                 list(range(len(self.labels))),
                 test_size=n_remove,
                 stratify=self.labels,
                 random_state=seed,
                 shuffle=True,
             )
-            self.image_paths = self.image_paths[idx]
+            self.images = self.images[idx]
             self.labels = self.labels[idx]
-
-        self.image_paths = self.image_paths.tolist()
-        self.labels = self.labels.tolist()
 
     def __len__(self):
         return len(self.labels)
 
     def __getitem__(self, idx):
-        image_path = os.path.join(self.image_paths[idx])
+        return self.images[idx], self.labels[idx].unsqueeze(-1)
+
+    def __loaditem__(self, idx):
+        image_path = self.data.loc[idx, 'file_path']
         image = Image.open(image_path)
         image = image.convert('RGB')
-        label = torch.tensor(self.labels[idx]).unsqueeze(-1)
         if self.transform:
             image = self.transform(image)
+        label = self.data.loc[idx, 'covid19_positive']
         return image, label
