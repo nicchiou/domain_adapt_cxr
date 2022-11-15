7134ffd5d5ed9f3b38a0931c4d010ef07e775dd3
Nicole Chiou <nicchiou@hyperion3.stanford.edu>
2022-11-15 14:50:17 -0800

    NC: Initial commit of train FiLM model on the source domain shell strcipt.

---
+++ jobs/train_source_film.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+log_dir='jobs/logs/'
+
+seed=(0 1 2 3 4)
+test_state=('CA' 'IN' 'NC' 'TX')
+
+for i in ${!seed[@]}; do
+for j in ${!test_state[@]}; do
+
+exp_dir='film_resnet50_hidden-1024_source-IL_target-'${test_state[$j]}
+
+echo $exp_dir
+
+python ~/domain_adapt_cxr/approaches/midrc/train.py \
+    --exp_dir $exp_dir \
+    --log_dir $log_dir \
+    --gpus 0 2 \
+    --train_state IL \
+    --test_state ${test_state[$j]} \
+    --n_samples -1 \
+    --domain source \
+    --resnet resnet50 \
+    --hidden_size 1024 \
+    --film_mode all \
+    --bn_replace 0 1 2 3 \
+    --epochs 100 \
+    --lr 0.001 \
+    --batch_size 128 \
+    --seed ${seed[$i]} \
+    --verbose;
+
+done
+done
