de73cd7f642e274c378555c0b49af1a6f7dcec05
Nicole Chiou <nicchiou@hyperion3.stanford.edu>
2022-11-18 14:47:40 -0800

    NC: Initial commit of shell script to tune hyperparameters for training target NC ERM model.

---
+++ jobs/train_NC_hyperparam_sweep.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+res_dir='NC_ERM_hyperparam_search/early_stop_auc/'
+log_dir='jobs/logs/'
+approach='Target_ERM'
+
+lr=(0.01 0.006 0.003 0.001 0.0006 0.0003 0.0001 0.00006 0.00003 0.00001)
+batch_size=(64 128)
+seed=(0 1 2 3 4)
+
+for i in ${!seed[@]}; do
+for j in ${!lr[@]}; do
+for k in ${!batch_size[@]}; do
+
+exp_dir='resnet50_lr-'${lr[$j]}'_batch-'${batch_size[$k]}'_source-IL_target-NC_ns-0_nt-all'
+
+echo $exp_dir
+
+python ~/domain_adapt_cxr/approaches/midrc/train.py \
+    --root_dir $res_dir \
+    --exp_dir $exp_dir \
+    --log_dir $log_dir \
+    --approach $approach \
+    --gpus 0 1 2 3 \
+    --train_state NC \
+    --test_state IL \
+    --n_samples -1 \
+    --domain source \
+    --resnet resnet50 \
+    --hidden_size 1024 \
+    --epochs 100 \
+    --lr ${lr[$j]} \
+    --batch_size ${batch_size[$k]} \
+    --seed ${seed[$i]} \
+    --early_stopping_metric auc \
+    --min_epochs 10 \
+    --verbose;
+
+done
+done
+done
