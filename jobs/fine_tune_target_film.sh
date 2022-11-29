81b493d628c77286a4135cf0716177a30757c78d
Nicole Chiou <nicchiou@hyperion3.stanford.edu>
2022-11-28 20:13:31 -0800

    NC: Initial commit of fine-tuning scripts.

--- jobs/fine_tune_target_film.sh
+++ jobs/fine_tune_target_film.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 root_dir='/dfs/scratch0/nicchiou/domain_adapt_cxr'
-res_dir='early_stop_auc/resnet/'
+res_dir='early_stop_auc/film_block-1234_bn-3/'
 log_dir='jobs/logs/'
-approach='Fine_Tune'
+approach='FiLM_Fine_Tune'
 
 resnet='resnet152'
 hidden_size=1024
+block_replace='1 2 3 4'
+bn_replace='3'
 model_path=$root_dir'/results/midrc/'$res_dir$resnet'_source-IL_target-CA_ns-all_nt-0'
 
 gpus='0 1 2 3'
@@ -34,6 +36,9 @@ for i in ${!seed[@]}; do
                 --domain target \
                 --resnet $resnet \
                 --hidden_size $hidden_size \
+                --film \
+                --block_replace $block_replace \
+                --bn_replace $bn_replace \
                 --load_pretrained $model_fname \
                 --fine_tune_modules \
                     resnet.conv1 resnet.bn1 \
@@ -47,32 +52,35 @@ for i in ${!seed[@]}; do
                 --verbose
         done
 
-        exp_dir=$resnet'_source-IL_target-'${train_state[$j]}'_ft-all_ns-all_nt-all'
-        model_fname=$model_path'/source_checkpoint_'${seed[$i]}'.pt'
-        echo $exp_dir
+    exp_dir=$resnet'_source-IL_target-'${train_state[$j]}'_ft-all_ns-all_nt-all'
+    model_fname=$model_path'/source_checkpoint_'${seed[$i]}'.pt'
+    echo $exp_dir
 
-        python ~/domain_adapt_cxr/approaches/midrc/train.py \
-            --root_dir $res_dir \
-            --exp_dir $exp_dir \
-            --log_dir $log_dir \
-            --approach $approach \
-            --gpus $gpus \
-            --train_state ${train_state[$j]} \
-            --test_state IL \
-            --n_samples -1 \
-            --domain target \
-            --resnet $resnet \
-            --hidden_size $hidden_size \
-            --load_pretrained $model_fname \
-            --fine_tune_modules \
-                resnet.conv1 resnet.bn1 \
-                resnet.layer1 resnet.layer2 resnet.layer3 resnet.layer4 \
-                resnet.fc linear \
-            --epochs 100 \
-            --lr 0.0003 \
-            --batch_size 64 \
-            --seed ${seed[$i]} \
-            --early_stopping_metric auc \
-            --verbose
+    python ~/domain_adapt_cxr/approaches/midrc/train.py \
+        --root_dir $res_dir \
+        --exp_dir $exp_dir \
+        --log_dir $log_dir \
+        --approach $approach \
+        --gpus $gpus \
+        --train_state ${train_state[$j]} \
+        --test_state IL \
+        --n_samples -1 \
+        --domain target \
+        --resnet $resnet \
+        --hidden_size $hidden_size \
+        --film \
+        --block_replace $block_replace \
+        --bn_replace $bn_replace \
+        --load_pretrained $model_fname \
+        --fine_tune_modules \
+            resnet.conv1 resnet.bn1 \
+            resnet.layer1 resnet.layer2 resnet.layer3 resnet.layer4 \
+            resnet.fc linear \
+        --epochs 100 \
+        --lr 0.0003 \
+        --batch_size 64 \
+        --seed ${seed[$i]} \
+        --early_stopping_metric auc \
+        --verbose
     done
 done
