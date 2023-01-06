708c8cf5420d34bfdee95b5f9fed1055a800d6ec
Nicole Chiou <nicchiou@hyperion3.stanford.edu>
2023-01-06 15:32:28 -0800

    NC: Adding resnet block sizes to constants.

--- libraries/midrc/utils/constants.py
+++ libraries/midrc/utils/constants.py
@@ -9,3 +9,11 @@ DATA_DIR = os.path.join(LFS, 'MIDRC', 'data_subset', 'cr', 'clean_states')
 METADATA_DIR = os.path.join(LFS, 'MIDRC', 'meta_info')
 
 RESULTS_DIR = os.path.join(DFS, 'domain_adapt_cxr', 'results', 'midrc')
+
+RESNET_BLOCK_SIZES = {
+        'resnet18': (2, 2, 2, 2),
+        'resnet34': (3, 4, 6, 3),
+        'resnet50': (3, 4, 6, 3),
+        'resnet101': (3, 4, 23, 3),
+        'resnet152': (3, 8, 36, 3),
+    }
