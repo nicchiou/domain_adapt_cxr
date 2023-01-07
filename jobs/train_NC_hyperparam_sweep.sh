65c2a7f847ba49b771b719b9d0bd25ea4c560288
Nicole Chiou <nicchiou@hyperion3.stanford.edu>
2023-01-07 10:48:42 -0800

    NC: NC hyperparm sweep with correct res_dir.

--- jobs/train_NC_hyperparam_sweep.sh
+++ jobs/train_NC_hyperparam_sweep.sh
@@ -2,7 +2,7 @@
 
 res_dir='NC_ERM_hyperparam_search/early_stop_auc/'
 log_dir='jobs/logs/'
-approach='Target_ERM'
+approach='Target_ERM_ResNet'
 
 resnet='resnet50'
 hidden_size=1024
@@ -15,11 +15,11 @@ seed=(0 1 2 3 4)
 for i in ${!seed[@]}; do
     for j in ${!lr[@]}; do
         for k in ${!batch_size[@]}; do
-            exp_dir=$resnet'_lr-'${lr[$j]}'_batch-'${batch_size[$k]}'_source-IL_target-NC_ns-0_nt-all'
+            exp_dir=$resnet'_lr-'${lr[$j]}'_batch-'${batch_size[$k]}'_source-IL_target-NC_ft-all_ns-0_nt-all'
             echo $exp_dir
 
             python ~/domain_adapt_cxr/approaches/midrc/train.py \
-                --root_dir $res_dir \
+                --res_dir $res_dir \
                 --exp_dir $exp_dir \
                 --log_dir $log_dir \
                 --approach $approach \
