74a821e340f4ea7dff0f97355040516879969ec3
Nicole Chiou <nicchiou@hyperion3.stanford.edu>
2023-01-07 13:51:26 -0800

    NC: Train target scripts with `batch_size=128`.

--- jobs/train_target.sh
+++ jobs/train_target.sh
@@ -2,7 +2,7 @@
 
 res_dir='early_stop_auc/resnet/'
 log_dir='jobs/logs/'
-approach='Target_ERM'
+approach='Target_ERM_ResNet'
 
 resnet='resnet152'
 hidden_size=1024
@@ -13,11 +13,11 @@ train_state=('CA' 'IN' 'TX')
 
 for i in ${!seed[@]}; do
     for j in ${!train_state[@]}; do
-        exp_dir=$resnet'_source-'${train_state[$j]}'_target-IL_ns-all_nt-0'
+        exp_dir=$resnet'_source-'${train_state[$j]}'_target-IL_ft-all_ns-all_nt-0'
         echo $exp_dir
 
         python ~/domain_adapt_cxr/approaches/midrc/train.py \
-            --root_dir $res_dir \
+            --res_dir $res_dir \
             --exp_dir $exp_dir \
             --log_dir $log_dir \
             --approach $approach \
@@ -30,7 +30,7 @@ for i in ${!seed[@]}; do
             --hidden_size $hidden_size \
             --epochs 100 \
             --lr 0.001 \
-            --batch_size 64 \
+            --batch_size 128 \
             --seed ${seed[$i]} \
             --early_stopping_metric auc \
             --min_epochs 10 \
--- jobs/train_target_film.sh
+++ jobs/train_target_film.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-res_dir='early_stop_auc/film_block-1234_bn-0123/'
+res_dir='early_stop_auc/film_block-1234_bn-3_all/'
 log_dir='jobs/logs/'
 approach='FiLM_Target_ERM'
 
 resnet='resnet152'
 hidden_size=1024
 block_replace='1 2 3 4'
-bn_replace='0 1 2 3'
+bn_replace='3'
 
 gpus='0 1 2 3'
 seed=(0 1 2 3 4)
-train_state=('CA' 'IN' 'NC' 'TX')
+train_state=('CA' 'IN' 'TX')
 
 for i in ${!seed[@]}; do
     for j in ${!train_state[@]}; do
-        exp_dir=$resnet'_source-'${train_state[$j]}'_target-IL_ns-all_nt-0'
+        exp_dir=$resnet'_source-'${train_state[$j]}'_target-IL_ft-all_ns-all_nt-0'
         echo $exp_dir
 
         python ~/domain_adapt_cxr/approaches/midrc/train.py \
-            --root_dir $res_dir \
+            --res_dir $res_dir \
             --exp_dir $exp_dir \
             --log_dir $log_dir \
             --approach $approach \
@@ -35,7 +35,7 @@ for i in ${!seed[@]}; do
             --bn_replace $bn_replace \
             --epochs 100 \
             --lr 0.001 \
-            --batch_size 64 \
+            --batch_size 128 \
             --seed ${seed[$i]} \
             --early_stopping_metric auc \
             --min_epochs 10 \
